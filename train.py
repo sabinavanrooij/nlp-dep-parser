354a6ad710757c97ff1aeb337eccca157c01f325
bobvandenhoogen <bobvandenhoogen@hotmail.com>
2017-12-12 22:53:40 +0100

    added unique label count parameter to the model

--- train.py
+++ train.py
@@ -45,6 +45,7 @@ wordsModel = Word2Vec(sentencesInWords, size=word_embeddings_dim, window=5, min_
 # LSTM training prep
 vocabularySize = len(w2i)
 tagsUniqueCount = len(t2i)
+labelsUniqueCount = len(l2i)
 
 pretrainedWordEmbeddings = np.empty((vocabularySize, word_embeddings_dim))
 for k,v in i2w.items():
@@ -61,7 +62,7 @@ for k,v in i2t.items():
     pretrainedTagEmbeddings[k] = POSTagsModel[v]
 
 
-model = DependencyParseModel(word_embeddings_dim, posTags_embeddings_dim, vocabularySize, tagsUniqueCount, pretrainedWordEmbeddings, pretrainedTagEmbeddings)
+model = DependencyParseModel(word_embeddings_dim, posTags_embeddings_dim, vocabularySize, tagsUniqueCount, labelsUniqueCount, pretrainedWordEmbeddings, pretrainedTagEmbeddings)
 parameters = filter(lambda p: p.requires_grad, model.parameters())
 parameters = nn.ParameterList(list(parameters))
 optimizer = torch.optim.Adam(parameters, lr=0.01, weight_decay=1E-6)
